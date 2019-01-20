421a50329868fe365bb81bc821e5700eca2bb693
MattMoony <monschein.matthias@gmail.com>
2019-01-20 02:37:18 +0100

    more functionality ...

--- src/Client.java
+++ src/Client.java
@@ -4,33 +4,60 @@ import java.util.stream.Stream;
 
 public class Client implements Runnable {
     private Thread thread;
+    private Server mother;
 
     private Socket cSocket;
     private PrintWriter out;
     private BufferedReader in;
 
-    public Client(Socket cSocket) throws IOException {
+    public Client(Socket cSocket, Server mom) throws IOException {
         this.cSocket = cSocket;
         this.out = new PrintWriter(cSocket.getOutputStream(), true);
         this.in = new BufferedReader(new InputStreamReader(cSocket.getInputStream()));
+        this.mother = mom;
     }
 
-    @Override
-    public void run() {
-        String request = "";
+    private String urlDecode(String url) {
+        if (url.indexOf('%') < 0)
+            return url;
 
+        int p = url.indexOf('%');
+        String str = url.substring(0, p);
+
+        while (++p < url.length()) {
+            str += (char) Integer.parseInt(url.substring(p, p+2), 16);
+            p++;
+
+            while (++p < url.length() && url.charAt(p) != '%')
+                str += url.charAt(p);
+        }
+
+        return str;
+    }
+
+    private String urlEncode (String str) {
+        char[] repl = {'?', '#', '[', ']', '@', '!', '$', '&', '\'', '(', ')', '*', '+', ',', ';', '=', '%', '{', '}'};
+
+        for (char r : repl)
+            str = str.replace(r+"", "%" + Integer.toString((int)r, 16).toUpperCase());
+        return str;
+    }
+
+    private void handleRequest() {
         try {
-            char[] buff = new char[1];
-            this.in.read(buff);
+            String[] fHead = this.in.readLine().split(" ");
 
-            System.out.println("now");
             this.out.println("HTTP/1.1 200 OK\r\nContent-type: text/html; charset=\"utf-8\"\r\n\r\n<!DOCTYPE html><html><head></head><body><h1>Hello World!</h1></body></html>");
         } catch (IOException e) {
             System.out.println(" [ERROR]: IOException ... ");
             System.out.println(e.getMessage());
         }
+    }
 
-        System.out.println(" [+] Done!");
+    @Override
+    public void run() {
+        System.out.println(" [" + this.mother.sName + "]: NEW REQUEST FROM " + this.cSocket.getInetAddress().toString().substring(1) + " ... ");
+        this.handleRequest();
     }
 
     public void start() {
--- src/Server.java
+++ src/Server.java
@@ -1,21 +1,33 @@
 import java.net.*;
 import java.io.*;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Server {
+    public String sName = "SERVER";
+
     private int port;
     private ArrayList<Client> clients = new ArrayList<>();
+    private HashMap<String, HashMap<String, String>> paths = new HashMap<>();
 
     public Server (int port) {
         this.port = port;
+
+        this.paths.put("GET", new HashMap<>());
+        this.paths.put("POST", new HashMap<>());
+    }
+    public Server(int port, String serverName) {
+        this(port);
+        this.sName = serverName;
     }
 
     public void start() {
         try ( ServerSocket sSocket = new ServerSocket(this.port) ) {
-            System.out.println(" [SERVER]: Now listening on :" + this.port + " ... ");
+            System.out.println(" [" + this.sName + "]: Now listening on :" + this.port + " ... ");
 
             while (true) {
-                Client c = new Client(sSocket.accept());
+                Client c = new Client(sSocket.accept(), this);
                 this.clients.add(c);
                 c.start();
             }
@@ -25,8 +37,18 @@ public class Server {
         }
     }
 
+    public void get(String urlPath, String filePath) {
+        this.set("GET", urlPath, filePath);
+    }
+    public void post(String urlPath, String filePath) {
+        this.set("POST", urlPath, filePath);
+    }
+    public void set(String method, String urlPath, String filePath) {
+        this.paths.get(method).put(urlPath, filePath);
+    }
+
     public static void main(String[] args) {
-        Server webServer = new Server(80);
+        Server webServer = new Server(80, "HTTP-SERVER");
         webServer.start();
     }
 }
